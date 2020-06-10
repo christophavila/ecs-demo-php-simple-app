6502918859bb7f387cba158d42f7cc9aaef1ba1b
Christopher Avila <avila.christopher@gmail.com>
2020-06-10 06:51:16 -0500

    Update inddex.php

--- src/index.php
+++ src/index.php
@@ -15,8 +15,8 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Simple PHP App</h1>
-                <h2>Congratulations</h2>
+                <h1>Aplicacion simple PHP corriendo sobre Contenedores ECS</h1>
+                <h2>Felicidades</h2>
                 <p>Your PHP application is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
